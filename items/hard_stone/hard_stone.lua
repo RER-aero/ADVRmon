502a1da76b0bfe7f276fff762027e23213f8f223
RER-aero <arijacob09@icloud.com>
2026-06-19 11:16:10 -0400

    Hard stone boosts stone type pokemon

---
+++ items/hard_stone/hard_stone.lua
@@ -0,0 +1,16 @@
+function ADVR.onLoad()
+    pickup.name = "Hard Stone"
+    pickup.desc = " An item to be held by a Pokémon. It's a durable stone that boosts the power of the holder's Rock-type attacks. "
+	pickup.weight = 85
+	pickup.tier = 1
+	pickup.maxAmount = 10
+	pickup.amountUses = -1
+	pickup.price = 35
+	pickup.spawnsIn = {relicPool.SECRET, relicPool.SHOP, relicPool.PODEST}
+	pickup.supportedInMultiplayer = true
+end
+
+function ADVR.onPickup()
+    pickup.RegisterItem()
+end
+
