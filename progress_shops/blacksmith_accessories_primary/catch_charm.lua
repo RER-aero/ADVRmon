2c02d31a77de84e6af079a33758877ec83a9867d
RER-aero <arijacob09@icloud.com>
2026-06-21 19:52:50 -0400

    New charms

---
+++ progress_shops/blacksmith_accessories_primary/catch_charm.lua
@@ -0,0 +1,13 @@
+function ADVR.onLoad()
+    progress.name = "Catch Charm"
+    progress.desc = "Increases base chance to catch enemies slightly."
+	progress.price = 8
+	progress.currency = "beastblood"
+	progress.priority = -2000
+
+	
+end
+
+function ADVR.ProgressEvents.onBuy()
+end
+
---
+++ progress_shops/blacksmith_accessories_primary/survival_charm.lua
@@ -0,0 +1,13 @@
+function ADVR.onLoad()
+    progress.name = "Survival Charm"
+    progress.desc = "When you have a pokemon active, gain an extra heart container"
+	progress.price = 10
+	progress.currency = "beastblood"
+	progress.priority = -2000
+
+	
+end
+
+function ADVR.ProgressEvents.onBuy()
+end
+
