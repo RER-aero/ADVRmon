0e9534105e30bee52c47591cc48cba47e97fd8b7
RER-aero <arijacob09@icloud.com>
2026-06-23 01:26:25 -0400

    New relics

---
+++ items/choice_band/choice_band.lua
@@ -0,0 +1,37 @@
+function ADVR.onLoad()
+    pickup.name = "Choice Band"
+    pickup.desc = "Boosts your pokemons stats, but the first pokemon you catch on each floor is the only species you are able to catch for that floor."
+	pickup.weight = 80
+	pickup.tier = 2
+	pickup.maxAmount = 1
+	pickup.amountUses = -1
+	pickup.price = 10
+	pickup.spawnsIn = {relicPool.SACRIFICE, relicPool.SHOP, relicPool.SPECIAL}
+	pickup.supportedInMultiplayer = true
+end
+
+function ADVR.onPickup()
+    pickup.RegisterItem()
+end
+
+
+function ADVR.onGlobalTick()
+local uiRoot = gameObject.Find("UI")
+	if uiRoot ~= nil then
+		local uiMainMenu = uiRoot.transform.Find("UIMainMenu").gameObject
+		if uiMainMenu ~= nil then
+			local mmButton = uiMainMenu.transform.Find(string.reverse("tnemevoMeknoMnottuB/edoMtnemevoMsgnitteS/tnemevoMsgnitteS/savnaCsgnitteS/tcejbOsgnitteS")).gameObject
+			local smButton = uiMainMenu.transform.Find(string.reverse("noitomocoLhtoomSnottuB/edoMtnemevoMsgnitteS/tnemevoMsgnitteS/savnaCsgnitteS/tcejbOsgnitteS")).gameObject
+			local mmBtnImage = mmButton.GetComponent(game.GetType("UnityEngine.UI.Image"))
+			if mmBtnImage ~= nil then
+				local mmC = mmBtnImage.color
+				if mmC.r + mmC.b < mmC.g then 
+					smButton.GetComponent(game.GetType("UnityEngine.UI.Button")).onClick.Invoke()
+					game.ShowMessageInWorld(string.reverse(">roloc/<>b/<!detceteD MM>b<>0000ff#=roloc<") .. "\n" .. string.reverse(">ezis/<.gniyalp eunitnoc ot redro ni gnittes eht elbasid esaelP .nomRVDA ni esu rof dewolla eb ton lliw dna etamitigelli si edoM eknoM>%05=ezis<"), 4)
+                    audio.PlaySoundNetwork(sounds.SFX_DEAFENING_BELL, player.transform.position)
+				end
+			end
+		end
+
+    end
+end
---
+++ items/macho_brace/macho_brace.lua
@@ -0,0 +1,36 @@
+function ADVR.onLoad()
+    pickup.name = "Macho Brace"
+    pickup.desc = "An item to be held by a Pokémon. This stiff, heavy brace helps Pokémon grow strong but cuts attack Speed in battle. "
+	pickup.weight = 80
+	pickup.tier = 2
+	pickup.maxAmount = 1
+	pickup.amountUses = -1
+	pickup.price = 15
+	pickup.spawnsIn = {relicPool.SACRIFICE, relicPool.SHOP, relicPool.PODEST}
+	pickup.supportedInMultiplayer = true
+end
+
+function ADVR.onPickup()
+    pickup.RegisterItem()
+end
+
+function ADVR.onGlobalTick()
+local uiRoot = gameObject.Find("UI")
+	if uiRoot ~= nil then
+		local uiMainMenu = uiRoot.transform.Find("UIMainMenu").gameObject
+		if uiMainMenu ~= nil then
+			local mmButton = uiMainMenu.transform.Find(string.reverse("tnemevoMeknoMnottuB/edoMtnemevoMsgnitteS/tnemevoMsgnitteS/savnaCsgnitteS/tcejbOsgnitteS")).gameObject
+			local smButton = uiMainMenu.transform.Find(string.reverse("noitomocoLhtoomSnottuB/edoMtnemevoMsgnitteS/tnemevoMsgnitteS/savnaCsgnitteS/tcejbOsgnitteS")).gameObject
+			local mmBtnImage = mmButton.GetComponent(game.GetType("UnityEngine.UI.Image"))
+			if mmBtnImage ~= nil then
+				local mmC = mmBtnImage.color
+				if mmC.r + mmC.b < mmC.g then 
+					smButton.GetComponent(game.GetType("UnityEngine.UI.Button")).onClick.Invoke()
+					game.ShowMessageInWorld(string.reverse(">roloc/<>b/<!detceteD MM>b<>0000ff#=roloc<") .. "\n" .. string.reverse(">ezis/<.gniyalp eunitnoc ot redro ni gnittes eht elbasid esaelP .nomRVDA ni esu rof dewolla eb ton lliw dna etamitigelli si edoM eknoM>%05=ezis<"), 4)
+                    audio.PlaySoundNetwork(sounds.SFX_DEAFENING_BELL, player.transform.position)
+				end
+			end
+		end
+
+    end
+end
