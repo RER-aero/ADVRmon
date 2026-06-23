f75caa46a6cc48d73fafb2b2f96917868fb00f82
RER-aero <arijacob09@icloud.com>
2026-06-23 01:26:02 -0400

    cathc all the types

---
+++ achievements/type_master.lua
@@ -0,0 +1,33 @@
+function ADVR.onLoad()
+    achievement.psvrID = 457
+    achievement.name = "Type Master"
+    achievement.desc = "Catch every type <color=#c90808>(ADV</color><color=#1c1b1a>R</color><color=#cecece>MON)</color>"
+	achievement.hideDescription = false
+    achievement.predecessors = {achievements.GOOD_LUCK}
+    achievement.showProgressBar = true
+    achievement.category = "exploration"
+    achievement.unlocksItem = "toxic_orb"
+end
+
+
+function ADVR.onSpawnInHomeBase()
+    if HasCaughtEveything() == true then
+        achievement.Unlock()
+    end
+end
+
+function GetProgressInfo()
+    local collected = GetList("TypesCaught")
+
+    return #collected, 12
+end
+
+function GetList(key)
+    local raw = game.LoadString(key, "")
+    if raw == "" or raw == nil then return {} end
+    local list = {}
+    for entry in raw:gmatch("[^,]+") do
+        table.insert(list, entry)
+    end
+    return list
+end
