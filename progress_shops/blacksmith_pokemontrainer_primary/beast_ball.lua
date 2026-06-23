5114c734420bcb0f4cbaee9cc8695864ecf182d3
RER-aero <arijacob09@icloud.com>
2026-06-23 01:25:29 -0400

    Beast Ba;; and safari ball

---
+++ progress_shops/blacksmith_pokemontrainer_primary/beast_ball.lua
@@ -0,0 +1,11 @@
+function ADVR.onLoad()
+    progress.name = "Beast Ball"
+    progress.desc = "Increases catch chance when you are on alternate floors"
+	progress.price = 25
+	progress.currency = "gold"
+   
+end
+
+function ADVR.ProgressEvents.onBuy()
+end
+
---
+++ progress_shops/blacksmith_pokemontrainer_primary/safari_ball.lua
@@ -0,0 +1,11 @@
+function ADVR.onLoad()
+    progress.name = "Safari Ball"
+    progress.desc = "Catch chance is increased massivley when in a POI"
+	progress.price = 35
+	progress.currency = "gold"
+    
+end
+
+function ADVR.ProgressEvents.onBuy()
+end
+
