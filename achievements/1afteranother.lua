1eaee4cfa7c9004ec461ecf41f978ec604a1656d
RER-aero <arijacob09@icloud.com>
2026-07-03 13:07:24 -0400

    updated desc

--- achievements/1afteranother.lua
+++ achievements/1afteranother.lua
@@ -1,7 +1,7 @@
 function ADVR.onLoad()
     achievement.psvrID = 160
     achievement.name = "One After Another"
-    achievement.desc = "Catch 5 enemies in a row <color=#c90808>(ADV</color><color=#1c1b1a>R</color><color=#cecece>MON)</color>"
+    achievement.desc = "Catch 5 enemies in a row without failing a catch<color=#c90808>(ADV</color><color=#1c1b1a>R</color><color=#cecece>MON)</color>"
     achievement.predecessors = { achievements.GOOD_LUCK, }
     achievement.category = "combat"
     achievement.unlocksItem = "choice_band"
