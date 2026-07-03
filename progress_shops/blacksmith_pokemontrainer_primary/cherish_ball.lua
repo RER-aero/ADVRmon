a6c043789ecd2172b5f5600434a0d02dc1b15a7b
RER-aero <arijacob09@icloud.com>
2026-07-02 23:15:03 -0400

    cherish ball and beast ball sprite update

---
+++ progress_shops/blacksmith_pokemontrainer_primary/cherish_ball.lua
@@ -0,0 +1,11 @@
+function ADVR.onLoad()
+    progress.name = "Cherish Ball"
+    progress.desc = "<color=#c8960c>Sh</color><color=#e8b84b>in</color><color=#f5d78e>y</color> enemies have a 100% chance to be caught"
+	progress.price = 50
+	progress.currency = "gold"
+   
+end
+
+function ADVR.ProgressEvents.onBuy()
+end
+
